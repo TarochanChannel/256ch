76348a2e8a2ec340b0747f73df1a5595c9e81a0b
TaroPP(太郎ちゃんチャンネル) <56919233+TarochanChannel@users.noreply.github.com>
2020-08-16 02:57:36 +0900

    前回の更新の修正2

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ client.on("message", message => {
         message.author.send("256chへようこそ。");
         codes[message.author.id] = code
         message.author.send("あなたの認証コードは、`256!" + code + "`です。\n5分ごとにリセットされるので、リセットされた場合は`256!getcode`で新しい認証コードを取得してください。");
-    } else if (message.content == "256!" + code[message.author.id]) {
+    } else if (message.content == "256!" + codes[message.author.id]) {
         hook.send(`${message.author.tag}さん、認証に成功しました。\nどうぞ256chをご利用ください。`);
         message.author.roles.add("744221904986177558");
         client.members.cache.get("698395012219666432").send(`${message.author.tag}(${message.author.id})が認証`);
